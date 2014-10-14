a4830f3195062b12b101180bdd58b2b1440cd2e2
Jameson Knoll <j.knoll621@gmail.com>
2014-10-14 18:36:35 +0800

    added directives

--- js/controllers/inbox.js
+++ js/controllers/inbox.js
@@ -2,17 +2,9 @@
 
 angular.module('EmailApp')
 	.controller('InboxCtrl',
-	function InboxCtrl ( $scope, InboxFactory ) {
+	function InboxCtrl ( $scope ) {
 		'use strict';
 		$scope.meta = {
 			title: "My Inbox"
 		};
-		InboxFactory.getMessages()
-			.success(function(jsonData, statusCode){
-				console.log('The request wasa successful!', statusCode);
-				console.dir(jsonData);
-				$scope.data = {
-					emails: jsonData
-				};
-			});
 	});
